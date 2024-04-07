4e7ff3c5e0454aca32c1b9814fc62dc5b05b1996
Nicolás Fuenzalida Saéz <nico_fuenza2000@hotmail.com>
2024-04-07 00:36:27 -0400

    Create setup.py

---
+++ 2022/Reinforcement learning para juego de cachos/Código/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Implementación de RL para el Juego de Cacho',
+    author='Javier Maass - Juan Pablo Sepúlveda',
+    license='MIT',
+)
